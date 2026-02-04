396e5b28c11151319cf7fb0734b38452c999573b
Basil <basilkb1992@gmail.com>
2026-02-05 01:33:38 +0530

    initial commit

---
+++ src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,18 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toPatientResponseDto(Patient  patient){
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(String.valueOf(patient.getId()));
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientResponseDTO;
+    }
+}
